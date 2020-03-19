d2f0aff7eca5b4a663321026d6ffb663ddea47ec
warren <warrenb95@live.co.uk>
2020-03-19 10:01:27 +0000

    works in docker!!

--- config/config.go
+++ config/config.go
@@ -8,9 +8,13 @@ import (
 // Templates obj for app.
 var Templates = template.Must(template.ParseGlob("templates/*.html"))
 
-// Url Strings for the app
-const GotodoURL string = "http://localhost"
+// GotodoURL Url for the app
+const GotodoURL string = "http://go_todo:80"
+
+// MongodbURL for the mongo API
 const MongodbURL string = "http://mongo_todo:3000/todo"
+
+// const MongodbURL string = "http://172.28.0.3:3000/todo"
 
 // Client http.client pointer
 var Client = &http.Client{}
